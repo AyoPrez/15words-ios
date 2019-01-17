939be4eb23c8a67043defc0750446153ad4fef3e
ayoze-perez-rodriguez <ayo@ayoprez.com>
2019-01-17 17:52:21 +0100

    Initial Commit

---
+++ 15Words/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  15Words
+//
+//  Created by Ayoze Pérez Rodríguez on 17.01.19.
+//  Copyright © 2019 AyoPrez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ 15WordsTests/_5WordsTests.swift
@@ -0,0 +1,34 @@
+//
+//  _5WordsTests.swift
+//  15WordsTests
+//
+//  Created by Ayoze Pérez Rodríguez on 17.01.19.
+//  Copyright © 2019 AyoPrez. All rights reserved.
+//
+
+import XCTest
+@testable import _5Words
+
+class _5WordsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
